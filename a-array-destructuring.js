39e6d19702f39d9104bb66eabfa4d716b9752c44
karanvirdhillon93 <77651757+karanvirdhillon93@users.noreply.github.com>
2021-08-02 15:47:18 -0700

    first commit

---
+++ a-array-destructuring.js
@@ -0,0 +1,11 @@
+const numbers = [ 1, 2, 3 ];
+
+console.log(numbers[0]);
+
+const [ x, y, z ] = numbers;
+
+console.log(x);
+
+const [ b, a, c ] = numbers;
+
+console.log(a);
---
+++ b-object-destructuring.js
@@ -0,0 +1,17 @@
+const rotation = { x: 3, y: 4, z: 2 };
+
+console.log('X rotation is ' + rotation.x + ' cm.');
+
+const { x, y, z } = rotation;
+
+console.log('X rotation is ' + x + ' cm.');
+
+const rotation2 = { a: 3, b: 4, c: 2 };
+
+const { b, a, c } = rotation2;
+
+console.log('The value of a is: ' + a);
+
+const { e, d, f } = rotation2;
+
+console.log('The value of d is: ' + d);
